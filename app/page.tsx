dbbe68debb718c5ae829859380dfdcc974f1f8cf
edr3x <dhunganaannanta@gmail.com>
2023-06-03 20:02:40 +0545

    test

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+// #005cda - Primary Button Color
+// #e2e6eb - Secondary Button Color
+// Font: Montserrat
+
+// Final Result: https://cdn.discordapp.com/attachments/1051161344222711808/1113721938498564107/e6356752b567a717f7b6e2c078cba601.png
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Begin Your Test</h1>
+    </main>
+  );
+}
